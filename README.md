3b57b2d0eafd6a4a1a3848ed6624a0883241e60b
Rahmat Aligos <artasikindonesia@gmail.com>
2016-03-27 21:00:31 +0700

    dadang gula

---
+++ README.md
@@ -0,0 +1,3 @@
+# authapi
+https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens
+Jswebtoken auth users api 
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'secret': 'aligosganteng',
+  'database': 'mongodb://user:password@mlab.com:25429/authapi'
+}
+
+// change database to your database, like mongolab or your local mongodb
